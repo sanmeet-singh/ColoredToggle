0cdab258d2122eaec5f92d8abd2d19fb034c110e
sanmeet-singh <snmt_singh@hotmail.com>
2019-05-07 17:45:26 +0530

    Colors are changing on Toggle

--- ColoredToggle/Assets/Scripts/ColoredToggleUI/ColoredToggle.cs
+++ ColoredToggle/Assets/Scripts/ColoredToggleUI/ColoredToggle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
@@ -11,7 +12,7 @@ namespace ColoredToggleUI
     /// </summary>
 
     [RequireComponent(typeof(RectTransform))]
-    public class ColoredToggle : MonoBehaviour
+    public class ColoredToggle : Selectable, IPointerClickHandler, ISubmitHandler, ICanvasElement
     {
         public enum ToggleTransition
         {
@@ -22,6 +23,9 @@ namespace ColoredToggleUI
         [Serializable]
         public class ToggleEvent : UnityEvent<bool>
         { }
+
+        public Color onColor;
+        public Color offColor;
 
         /// <summary>
         /// Transition type.
@@ -64,5 +68,118 @@ namespace ColoredToggleUI
         [SerializeField]
         private bool m_IsOn;
 
+        /// <summary>
+        /// Toggle state
+        /// </summary>
+        public bool isOn
+        {
+            get { return m_IsOn; }
+            set
+            {
+                Set(value);
+            }
+        }
+
+        void Set(bool value)
+        {
+            Set(value, true);
+        }
+
+        void Set(bool value, bool sendCallback)
+        {
+            if (m_IsOn == value)
+            {
+                UpdateColor();
+                return;
+            }
+
+            // if we are in a group and set to true, do group logic
+            m_IsOn = value;
+            //if (m_Group != null && IsActive())
+            //{
+            //    if (m_IsOn || (!m_Group.AnyTogglesOn() && !m_Group.allowSwitchOff))
+            //    {
+            //        m_IsOn = true;
+            //        m_Group.NotifyToggleOn(this);
+            //    }
+            //}
+
+            // Always send event when toggle is clicked, even if value didn't change
+            // due to already active toggle in a toggle group being clicked.
+            // Controls like SelectionList rely on this.
+            // It's up to the user to ignore a selection being set to the same value it already was, if desired.
+            //PlayEffect(toggleTransition == ToggleTransition.None);
+
+            UpdateColor();
+
+            if (sendCallback)
+                onValueChanged.Invoke(m_IsOn);
+        }
+
+        private void InternalToggle()
+        {
+            if (!IsActive() || !IsInteractable())
+                return;
+
+            isOn = !isOn;
+        }
+
+
+        /// <summary>
+        /// React to clicks.
+        /// </summary>
+        public virtual void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button != PointerEventData.InputButton.Left)
+                return;
+
+            InternalToggle();
+        }
+
+        public virtual void OnSubmit(BaseEventData eventData)
+        {
+            InternalToggle();
+        }
+
+        public void Rebuild(CanvasUpdate executing)
+        {
+
+#if UNITY_EDITOR
+            if (executing == CanvasUpdate.Prelayout)
+                onValueChanged.Invoke(m_IsOn);
+#endif
+        }
+
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            Set(m_IsOn, false);
+            //PlayEffect(toggleTransition == ToggleTransition.None);
+
+            var prefabType = UnityEditor.PrefabUtility.GetPrefabType(this);
+            if (prefabType != UnityEditor.PrefabType.Prefab && !Application.isPlaying)
+                CanvasUpdateRegistry.RegisterCanvasElementForLayoutRebuild(this);
+        }
+
+#endif // if UNITY_EDITOR
+
+
+        public void LayoutComplete()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void GraphicUpdateComplete()
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void UpdateColor()
+        {
+            Debug.Log("uc : " + m_IsOn);
+            this.transform.GetChild(0).GetComponent<Text>().color = m_IsOn ? onColor : offColor;
+        }
     }
 }
---
+++ ColoredToggle/Assets/Scripts/TestToggle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ColoredToggleUI;
+using UnityEngine;
+
+public class TestToggle : MonoBehaviour
+{
+
+    public ColoredToggle coloredToggle;
+
+    void Start()
+    {
+        //coloredToggle.onValueChanged.AddListener(delegate
+        //{
+        //    Ok(coloredToggle);
+        //});
+    }
+
+    public void Ok(ColoredToggle toggle)
+    {
+        Debug.Log("ColoredToggle : " + toggle.isOn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
